c379b41b4f60fd35c44c762cee30e3f10037f137
Nelson Hoang <omega@nelsons-air.lan>
2024-02-14 22:10:49 -0800

    Hello World Index Page

---
+++ site/src/jsMain/kotlin/com/omega/san/pages/Index.kt
@@ -0,0 +1,25 @@
+package com.omega.san.pages
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.core.Page
+import org.jetbrains.compose.web.dom.Text
+import com.varabyte.kobweb.worker.rememberWorker
+import com.omega.san.worker.EchoWorker
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Column
+
+@Page
+@Composable
+fun HomePage() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
