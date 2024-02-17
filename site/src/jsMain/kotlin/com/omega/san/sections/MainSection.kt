5186f56cd9448bbaa2fc2758966b1aca906527f0
NelsonH <nelson.hoang@gmail.com>
2024-02-16 17:32:18 -0800

    Create header compontent and define sections

--- site/src/jsMain/kotlin/com/omega/san/sections/MainSection.kt
+++ site/src/jsMain/kotlin/com/omega/san/sections/MainSection.kt
@@ -1,6 +1,8 @@
 package com.omega.san.sections
 
 import androidx.compose.runtime.Composable
+import com.omega.san.components.Header
+import com.omega.san.model.Section
 import com.omega.san.util.Constants.SECTION_WIDTH
 import com.omega.san.util.Res
 import com.varabyte.kobweb.compose.css.ObjectFit
@@ -8,6 +10,7 @@ import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.id
 import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.objectFit
 import com.varabyte.kobweb.silk.components.graphics.Image
@@ -16,16 +19,22 @@ import org.jetbrains.compose.web.css.px
 @Composable
 fun MainSection() {
     Box(
-        Modifier.maxWidth(SECTION_WIDTH.px), contentAlignment = Alignment.TopCenter
+        modifier = Modifier
+            .id(Section.Home.id)
+            .maxWidth(SECTION_WIDTH.px),
+        contentAlignment = Alignment.TopCenter
     ) {
         MainBackground()
+        Header()
     }
 }
 
 @Composable
 fun MainBackground() {
     Image(
-        modifier = Modifier.fillMaxSize().objectFit(ObjectFit.Cover),
+        modifier = Modifier
+            .fillMaxSize()
+            .objectFit(ObjectFit.Cover),
         src = Res.Image.BACKGROUND,
         description = "Background Image"
     )
--- site/src/jsMain/kotlin/com/omega/san/util/Constants.kt
+++ site/src/jsMain/kotlin/com/omega/san/util/Constants.kt
@@ -2,10 +2,12 @@ package com.omega.san.util
 
 object Constants {
     const val SECTION_WIDTH = 1920
+    const val FONT_FAMILY = "roboto"
 }
 
 object Res {
     object Image {
         const val BACKGROUND = "background.svg"
+        const val LOGO = "logo.svg"
     }
 }
