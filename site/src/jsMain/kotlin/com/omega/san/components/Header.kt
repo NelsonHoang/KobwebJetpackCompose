2165267361aedef626977eb39b963fd33ce78de1
NelsonH <nelson.hoang@gmail.com>
2024-02-17 09:15:20 -0800

    Add transition for header items

--- site/src/jsMain/kotlin/com/omega/san/components/Header.kt
+++ site/src/jsMain/kotlin/com/omega/san/components/Header.kt
@@ -3,9 +3,11 @@ package com.omega.san.components
 import androidx.compose.runtime.Composable
 import com.omega.san.model.Section
 import com.omega.san.model.Theme
+import com.omega.san.style.NavigationItemStyle
 import com.omega.san.util.Constants.FONT_FAMILY
 import com.omega.san.util.Res
 import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextDecorationLine
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -13,6 +15,7 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.toModifier
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
@@ -51,11 +54,12 @@ fun RightSide() {
         horizontalArrangement = Arrangement.End) {
         Section.entries.toTypedArray().take(6).forEach { section ->
             Link(
-                modifier = Modifier
+                modifier = NavigationItemStyle.toModifier()
                     .padding(right = 30.px)
                     .fontFamily(FONT_FAMILY)
                     .fontSize(18.px)
-                    .fontWeight(FontWeight.Normal),
+                    .fontWeight(FontWeight.Normal)
+                    .textDecorationLine(TextDecorationLine.None),
                 path = section.path,
                 text = section.title)
         }
