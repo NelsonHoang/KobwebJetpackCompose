10c8148b6afad859d3f92d254477673e45c75d60
NelsonH <nelson.hoang@gmail.com>
2024-02-19 21:55:11 -0800

    Adding text

--- site/src/jsMain/kotlin/com/omega/san/sections/MainSection.kt
+++ site/src/jsMain/kotlin/com/omega/san/sections/MainSection.kt
@@ -4,22 +4,34 @@ import androidx.compose.runtime.Composable
 import com.omega.san.components.Header
 import com.omega.san.components.SocialBar
 import com.omega.san.model.Section
+import com.omega.san.model.Theme
+import com.omega.san.util.Constants.FONT_FAMILY
+import com.omega.san.util.Constants.LOREM_IPSUM_TEXT
 import com.omega.san.util.Constants.SECTION_WIDTH
 import com.omega.san.util.Res
+import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.id
-import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.objectFit
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.P
+import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun MainSection() {
+    val breakpoint = rememberBreakpoint()
     Box(
         modifier = Modifier
             .id(Section.Home.id)
@@ -27,7 +39,7 @@ fun MainSection() {
         contentAlignment = Alignment.TopCenter
     ) {
         MainBackground()
-        MainContent()
+        MainContent(breakpoint = breakpoint)
     }
 }
 
@@ -43,12 +55,87 @@ fun MainBackground() {
 }
 
 @Composable
-fun MainContent() {
+fun MainContent(breakpoint: Breakpoint) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Header()
-        SocialBar()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SimpleGrid(
+                modifier = Modifier
+                    .fillMaxWidth(
+                        if (breakpoint >= Breakpoint.MD) 80.percent
+                        else 90.percent
+                    ),
+                numColumns = numColumns(base = 1, md = 2)
+            ) {
+                MainText(breakpoint)
+            }
+        }
+    }
+}
+
+@Composable
+fun MainText(breakpoint: Breakpoint) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (breakpoint > Breakpoint.MD) {
+            SocialBar()
+        }
+
+        Column {
+            P(
+                attrs = Modifier
+                    .margin(topBottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(45.px)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Primary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Hello, I am")
+            }
+            P(
+                attrs = Modifier
+                    .margin(top = 20.px, bottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(68.px)
+                    .fontWeight(FontWeight.Bolder)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Bob Mandela")
+            }
+            P(
+                attrs = Modifier
+                    .margin(top = 10.px, bottom = 5.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(30.px)
+                    .fontWeight(FontWeight.Bold)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Mobile & Web Developer/Designer")
+            }
+            P(
+                attrs = Modifier
+                    .margin(bottom = 25.px)
+                    .maxWidth(400.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(35.px)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text(LOREM_IPSUM_TEXT)
+            }
+        }
     }
 }
--- site/src/jsMain/kotlin/com/omega/san/util/Constants.kt
+++ site/src/jsMain/kotlin/com/omega/san/util/Constants.kt
@@ -7,6 +7,8 @@ object Constants {
     const val INSTAGRAM_URL = "https://www.instagram.com"
     const val LINKEDIN_URL = "https://www.linkedin.com"
     const val TWITTER_URL = "https://www.twitter.com"
+
+    const val LOREM_IPSUM_TEXT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis."
 }
 
 object Res {
