3d2b8652c622a0ecaf2792c3b9c611aab8690a17
NelsonH <nelson.hoang@gmail.com>
2024-02-20 21:54:41 -0800

    Adding hire me button

--- site/src/jsMain/kotlin/com/omega/san/sections/MainSection.kt
+++ site/src/jsMain/kotlin/com/omega/san/sections/MainSection.kt
@@ -11,21 +11,25 @@ import com.omega.san.util.Constants.SECTION_WIDTH
 import com.omega.san.util.Res
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.css.TextDecorationLine
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.layout.SimpleGrid
 import com.varabyte.kobweb.silk.components.layout.numColumns
+import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
@@ -135,6 +139,22 @@ fun MainText(breakpoint: Breakpoint) {
                     .toAttrs()
             ) {
                 Text(LOREM_IPSUM_TEXT)
+            }
+            Button (
+                attrs = Modifier
+                    .height(40.px)
+                    .borderRadius(r = 5.px)
+                    .backgroundColor(Theme.Primary.rgb)
+                    .color(Colors.White)
+                    .toAttrs()
+            ) {
+                Link(
+                   modifier = Modifier
+                       .color(Colors.White)
+                       .textDecorationLine(TextDecorationLine.None),
+                    text = "Hire me",
+                    path = Section.Contact.path
+                )
             }
         }
     }
