8b2d6b6887f0a8bb6e9d5313a51d7e57e7efb46d
NelsonH <nelson.hoang@gmail.com>
2024-02-29 10:09:30 -0800

    Adding animation to ServiceCard when a uesr hovers over it

--- site/src/jsMain/kotlin/com/omega/san/components/ServiceCard.kt
+++ site/src/jsMain/kotlin/com/omega/san/components/ServiceCard.kt
@@ -3,6 +3,7 @@ package com.omega.san.components
 import androidx.compose.runtime.Composable
 import com.omega.san.model.Service
 import com.omega.san.model.Theme
+import com.omega.san.style.ServiceCardStyle
 import com.omega.san.util.Constants
 import com.varabyte.kobweb.compose.css.FontStyle
 import com.varabyte.kobweb.compose.css.FontWeight
@@ -12,6 +13,7 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.style.toModifier
 import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
@@ -20,18 +22,14 @@ import org.jetbrains.compose.web.dom.Text
 @Composable
 fun ServiceCard(service: Service) {
     Column(
-        modifier = Modifier
+        modifier = ServiceCardStyle.toModifier()
             .maxWidth(300.px)
             .margin(all = 20.px)
             .padding(all = 20.px)
-            .border(
-                width = 2.px,
-                style = LineStyle.Solid,
-                color = Theme.LighterGray.rgb
-            )
     ) {
         Box(
             modifier = Modifier
+                .id("iconBox")
                 .padding(all = 10.px)
                 .margin(bottom = 20.px)
                 .border(
@@ -59,7 +57,6 @@ fun ServiceCard(service: Service) {
                 .fontSize(18.px)
                 .fontWeight(FontWeight.Bold)
                 .fontStyle(FontStyle.Italic)
-                .color(Theme.Secondary.rgb)
                 .toAttrs()
         ) {
             Text(service.title)
@@ -71,7 +68,6 @@ fun ServiceCard(service: Service) {
                 .fontSize(14.px)
                 .fontWeight(FontWeight.Normal)
                 .fontStyle(FontStyle.Italic)
-                .color(Theme.Secondary.rgb)
                 .toAttrs()
         ) {
             Text(service.description)
