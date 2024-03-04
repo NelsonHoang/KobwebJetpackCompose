dde949123533687d73698c7487c109d45e07bdc7
NelsonH <nelson.hoang@gmail.com>
2024-03-04 12:46:56 -0800

    Adding ExperienceSection, ExperienceCard and Experience Model

---
+++ site/src/jsMain/kotlin/com/omega/san/sections/ExperienceSection.kt
@@ -0,0 +1,62 @@
+package com.omega.san.sections
+
+import androidx.compose.runtime.*
+import com.omega.san.components.ExperienceCard
+import com.omega.san.components.SectionTitle
+import com.omega.san.model.Experience
+import com.omega.san.model.Section
+import com.omega.san.util.Constants
+import com.omega.san.util.Constants.SECTION_WIDTH
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun ExperienceSection() {
+    Box(
+        modifier = Modifier
+            .id(Section.Experience.id)
+            .maxWidth(SECTION_WIDTH.px)
+            .padding(topBottom = 100.px),
+        contentAlignment = Alignment.Center
+    ) {
+        ExperienceContent()
+    }
+}
+
+@Composable
+fun ExperienceContent() {
+    val breakpoint = rememberBreakpoint()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(
+                if (breakpoint >= Breakpoint.MD) 100.percent
+                else 90.percent
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SectionTitle(
+            modifier = Modifier
+                .fillMaxWidth(
+                    if (breakpoint >= Breakpoint.MD) 60.percent
+                    else 90.percent
+                )
+                .margin(bottom = 25.px),
+            section = Section.Experience
+        )
+        Experience.entries.forEach { experience ->
+            ExperienceCard(
+                breakpoint = breakpoint,
+                active = experience == Experience.First,
+                experience = experience
+            )
+        }
+
+    }
+}
--- site/src/jsMain/kotlin/com/omega/san/util/Constants.kt
+++ site/src/jsMain/kotlin/com/omega/san/util/Constants.kt
@@ -11,6 +11,8 @@ object Constants {
     const val TWITTER_URL = "https://www.twitter.com"
     const val WEBSITE = "https://stevdza-san.com"
 
+    const val LOREM_IPSUM_LONG =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const val LOREM_IPSUM_SHORTEST =
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     const val LOREM_IPSUM_SHORT =
