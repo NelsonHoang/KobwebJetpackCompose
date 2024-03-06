1b35ab9f62a29d75d2f4529eceaa64cd8750672c
NelsonH <nelson.hoang@gmail.com>
2024-03-06 12:51:23 -0800

    [SectionTitle] Adding animations

--- site/src/jsMain/kotlin/com/omega/san/components/SectionTitle.kt
+++ site/src/jsMain/kotlin/com/omega/san/components/SectionTitle.kt
@@ -1,9 +1,16 @@
 package com.omega.san.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import com.omega.san.model.Section
 import com.omega.san.model.Theme
 import com.omega.san.util.Constants.FONT_FAMILY
+import com.omega.san.util.ObserveViewPortEntered
+import com.varabyte.kobweb.compose.css.CSSTransition
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.foundation.layout.Box
@@ -12,6 +19,9 @@ import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.web.css.ms
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
@@ -22,20 +32,53 @@ fun SectionTitle(
     section: Section,
     alignment: Alignment.Horizontal = Alignment.Start
 ) {
+    val scope = rememberCoroutineScope()
+    var titleMargin by remember { mutableStateOf(50.px) }
+    var subtitleMargin by remember { mutableStateOf(50.px) }
+
+    ObserveViewPortEntered(
+        sectionId = section.id,
+        distanceFromTop = 700.0,
+        onViewportEntered = {
+            scope.launch {
+                subtitleMargin = 0.px
+                if (alignment == Alignment.Start) {
+                    delay(25)
+                }
+                titleMargin = 0.px
+            }
+        }
+    )
+
     Column(
-        modifier = modifier, horizontalAlignment = alignment) {
+        modifier = modifier,
+        horizontalAlignment = alignment
+    ) {
         P(
             attrs = Modifier
                 .fillMaxWidth()
                 .textAlign(
-                    if (alignment == Alignment.CenterHorizontally) TextAlign.Center
-                    else TextAlign.Start
+                    when (alignment) {
+                        Alignment.CenterHorizontally -> TextAlign.Center
+                        Alignment.End -> TextAlign.End
+                        else -> TextAlign.Start
+                    }
                 )
-                .margin(topBottom = 0.px)
+                .margin(
+                    left = titleMargin,
+                    top = 0.px,
+                    bottom = 0.px
+                )
                 .fontFamily(FONT_FAMILY)
                 .fontSize(25.px)
                 .fontWeight(FontWeight.Normal)
                 .color(Theme.Primary.rgb)
+                .transition(
+                    CSSTransition(
+                        property = "margin",
+                        duration = 300.ms
+                    )
+                )
                 .toAttrs()
         ) {
             Text(section.title)
@@ -44,14 +87,28 @@ fun SectionTitle(
             attrs = Modifier
                 .fillMaxWidth()
                 .textAlign(
-                    if (alignment == Alignment.CenterHorizontally) TextAlign.Center
-                    else TextAlign.Start
+                    when (alignment) {
+                        Alignment.CenterHorizontally -> TextAlign.Center
+                        Alignment.End -> TextAlign.End
+                        else -> TextAlign.Start
+                    }
                 )
-                .margin(bottom = 10.px, top = 0.px)
+                .margin(
+                    left = if (alignment == Alignment.Start) subtitleMargin else 0.px,
+                    right = if (alignment == Alignment.CenterHorizontally) subtitleMargin else 0.px,
+                    bottom = 10.px,
+                    top = 0.px
+                )
                 .fontFamily(FONT_FAMILY)
                 .fontSize(40.px)
                 .fontWeight(FontWeight.Bold)
                 .color(Theme.Secondary.rgb)
+                .transition(
+                    CSSTransition(
+                        property = "margin",
+                        duration = 300.ms
+                    )
+                )
                 .toAttrs()
         ) {
             Text(section.subtitle)
--- site/src/jsMain/kotlin/com/omega/san/sections/ContactSection.kt
+++ site/src/jsMain/kotlin/com/omega/san/sections/ContactSection.kt
@@ -1,10 +1,17 @@
 package com.omega.san.sections
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import com.omega.san.components.ContactForm
 import com.omega.san.components.SectionTitle
 import com.omega.san.model.Section
 import com.omega.san.util.Constants
+import com.omega.san.util.ObserveViewPortEntered
+import com.varabyte.kobweb.compose.css.CSSTransition
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -12,6 +19,10 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.web.css.deg
+import org.jetbrains.compose.web.css.ms
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
@@ -19,7 +30,7 @@ import org.jetbrains.compose.web.css.px
 fun ContactSection() {
     Box(
         modifier = Modifier
-            .id(Section.Experience.id)
+            .id(Section.Contact.id)
             .maxWidth(Constants.SECTION_WIDTH.px)
             .padding(topBottom = 100.px),
         contentAlignment = Alignment.Center
@@ -31,7 +42,20 @@ fun ContactSection() {
 @Composable
 fun ContactContent() {
     val breakpoint = rememberBreakpoint()
+    val scope = rememberCoroutineScope()
+    var animatedRotation by remember { mutableStateOf(0.deg) }
 
+    ObserveViewPortEntered(
+        sectionId = Section.Contact.id,
+        distanceFromTop = 500.0,
+        onViewportEntered = {
+            animatedRotation = 10.deg
+            scope.launch {
+                delay(500)
+                animatedRotation = 0.deg
+            }
+        }
+    )
     Column(
         modifier = Modifier
             .fillMaxWidth(
@@ -43,7 +67,14 @@ fun ContactContent() {
         SectionTitle(
             modifier = Modifier
                 .fillMaxWidth()
-                .margin(bottom = 25.px),
+                .margin(bottom = 25.px)
+                .transform { rotate(animatedRotation) }
+                .transition(
+                    CSSTransition(
+                        property = "transform",
+                        duration = 500.ms
+                    )
+                ),
             section = Section.Contact,
             alignment = Alignment.CenterHorizontally
         )
